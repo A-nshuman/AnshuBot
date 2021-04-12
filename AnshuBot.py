85f84b7babafdcdea378e2f29087eaf9d3cb5e9e
Anshuman Bhardwaj <64587347+1anshuman@users.noreply.github.com>
2021-04-12 11:19:57 +0300

    Update AnshuBot.py

--- AnshuBot.py
+++ AnshuBot.py
@@ -17,7 +17,6 @@ client = commands.Bot(command_prefix = '.', intents=intents)
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('with MrAnshuman#1060'))
     print('Bot is ready.')
 
 @client.event
